245fc5a5b0044d68c67ce0bdb5976aa5338ae868
HardikItaliya <hardik.gvtechnolab@gmail.com>
2023-12-22 16:14:46 +0530

    next config

---
+++ dist/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Ndd6OCdlNmsv8YqWQs8riU7BRp2TNpJhVFZ2Ko3HFwk=\"\n}"
--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-}
+  distDir: "dist",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
